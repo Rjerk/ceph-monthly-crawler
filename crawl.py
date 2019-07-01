3374c2ab9e725ee1cdc0de22e46e45aa086a25fd
Lan Liu <liulan@umcloud.com>
2019-07-01 20:47:34 +0800

    save all prs to excel file

--- crawl.py
+++ crawl.py
@@ -7,11 +7,12 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support import expected_conditions as expected
-# from selenium.webdriver.support.wait import WebDriverWait
+import xlsxwriter
+
+wb = xlsxwriter.Workbook("ceph-monthly.xlsx")
+ws = wb.add_worksheet(datetime.datetime.now().strftime("%B"))
+row = 0
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -21,53 +22,53 @@ driver = webdriver.Chrome(options=chrome_options)
 def get_html(url):
     try:
         driver.get(url)
-        # wait.until(expected.visibility_of_element_located((By.NAME, 'q'))).send_keys('headless firefox' + Keys.ENTER)
-        # wait.until(expected.visibility_of_element_located((By.CSS_SELECTOR, '#ires a'))).click()
     except:
         print ("Have a problem when get: {}".format(url), file=weird)
     time.sleep(2)
     return driver.page_source
 
-def this_month():
-    return datetime.datetime.now().month
+def this_month(month):
+    return month == datetime.datetime.now().month
 
-soup = BeautifulSoup(get_html('https://github.com/ceph/ceph/pulse/monthly'), 'html.parser')
+def get_soup(uri):
+    return BeautifulSoup(get_html(uri), 'html.parser')
 
-less  = open('less',  'w')
-more  = open('more',  'w')
-weird = open('weird', 'w')
+def write_file(row, pr_id, pr_title, pr_link, pr_merged_time, incre, decre):
+    ws.write(row, 0, pr_id)
+    ws.write(row, 1, pr_title)
+    ws.write(row, 2, pr_link)
+    ws.write(row, 3, pr_merged_time)
+    ws.write(row, 4, incre)
+    ws.write(row, 5, decre)
 
-for li in soup.find_all('li'):
+for li in get_soup('https://github.com/ceph/ceph/pulse/monthly').find_all('li'):
     if li.span and li.span.string and 'Merged' in li.span.string:
-        pr_merged_time = li.find('relative-time').get('datetime')
-        t = datetime.datetime.strptime(pr_merged_time, '%Y-%m-%dT%H:%M:%SZ')
+        pr_merged_time = datetime.datetime.strptime(
+                            li.find('relative-time').get('datetime'),
+                            '%Y-%m-%dT%H:%M:%SZ'
+                         )
 
-        if t.month == this_month():
-            pr_id = li.find_all('span', 'num')[0].string
-            pr_title = li.a.string
-            pr_link = 'https://github.com' + li.a.get('href')
-            print ("Hi, {}".format(pr_link))
+        if this_month(pr_merged_time.month):
 
-            new_soup = BeautifulSoup(get_html(pr_link), 'html.parser')
-            diff_stat = new_soup.find(id='diffstat')
+            pr_id    =  (li.find_all('span', 'num')[0].string)[1:]
+            pr_title =  li.a.string
+            pr_link  =  'https://github.com' + li.a.get('href')
+
+            print ("{} {} {} {} {}".format(pr_id, pr_title, pr_link, pr_merged_time.date(), pr_merged_time.month))
+
+            diff_stat = get_soup(pr_link).find(id='diffstat')
 
             if not diff_stat:
-                print ("Weird, no diff_stat ?", file=weird)
-                print ('[', pr_id, t.date(), ']', file=weird)
-                print (pr_title, '\n', pr_link, '\n', file=weird)
-                print (file=weird)
-                print (file=weird)
+                write_weird(row, pr_id, pr_title, pr_link, pr_merged_time.date(), None, None)
+                row += 1
                 continue
 
             incre = diff_stat.contents[1].string.strip().strip('+').replace(',', '')
             decre = diff_stat.contents[3].string.strip().strip('−').replace(',', '')
-            if 0 <= int(incre) <= 10 and 0 <= int(decre) <= 10:
-                print ('[', pr_id, t.date(), ']', file=less)
-                print (pr_title, '==> +{}'.format(incre), '-{}'.format(decre), \
-                       '\n', pr_link, '\n', file=less)
-            else:
-                print ('[', pr_id, t.date(), ']', file=more)
-                print (pr_title, '==> +{}'.format(incre), '-{}'.format(decre), \
-                       '\n', pr_link, '\n', file=more)
+
+            write_file(row, pr_id, pr_title, pr_link, pr_merged_time.date(), incre, decre)
+            row += 1
+            #break
 
 driver.quit()
+wb.close()
